a73637da3ef7775c6adb39ddac92522752c02ebc
mikolaj adamowicz <mr.ming122@gmail.com>
2018-03-26 10:41:47 +0200

    1 commit

--- Licencjat/ViewController.swift
+++ Licencjat/ViewController.swift
@@ -10,16 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var forButton: UIButton!
+    @IBOutlet weak var againstButton: UIButton!
+    @IBOutlet weak var liquidView: UIView!
+    @IBOutlet weak var buttonsStackView: UIStackView!
+    @IBOutlet weak var questionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        liquidView.frame.size.height = self.view.frame.height/4 * 3.25
+        liquidView.center.x = self.view.center.x
+        liquidView.center.y = self.view.frame.height * 2
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func changeFirstViewOpacity(isHidden:Bool){
+        if isHidden {
+            buttonsStackView.isHidden = true
+            questionLabel.isHidden = true
+        } else {
+            buttonsStackView.isHidden = false
+            questionLabel.isHidden = false
+        }
+    }
 
-
+    @IBAction func onClick(_ sender: Any) {
+        
+        UIView.animate(withDuration: 1.5, animations: {
+            self.changeFirstViewOpacity(isHidden: true)
+            self.liquidView.center.y = self.view.center.y + self.view.frame.height/4 * 0.75
+        }, completion: { finished in
+            print("View animated!")
+        })
+        
+        
+    }
+    
 }
 
